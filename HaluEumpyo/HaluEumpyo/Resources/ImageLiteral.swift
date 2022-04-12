96045a3e90e385406e26a1dc7d341da50f0e5774
bsorinnn <bxxsxrxx@gmail.com>
2022-04-12 23:58:12 +0900

    [ADD] ImageLiteral 추가 (#4)

---
+++ HaluEumpyo/HaluEumpyo/Resources/ImageLiteral.swift
@@ -0,0 +1,37 @@
+//
+//  ImageLiteral.swift
+//  HaluEumpyo
+//
+//  Created by 배소린 on 2022/04/12.
+//
+
+import UIKit
+
+public enum ImageLiteral {
+    public static let btnArchivebox: UIImage = UIImage(named: "btn_archivebox")!
+    public static let btnBack: UIImage = UIImage(named: "btn_back")!
+    public static let btnDown: UIImage = UIImage(named: "btn_down")!
+    public static let btnMove: UIImage = UIImage(named: "btn_move")!
+    public static let btnSend: UIImage = UIImage(named: "btn_send")!
+    public static let btnSetting: UIImage = UIImage(named: "btn_setting")!
+    public static let btnSendActivated: UIImage = UIImage(named: "btn_send_activated")!
+    
+    public static let bgEmotionAngry: UIImage = UIImage(named: "bg_angry")!
+    public static let bgEmotionFear: UIImage = UIImage(named: "bg_fear")!
+    public static let bgEmotionHate: UIImage = UIImage(named: "bg_hate")!
+    public static let bgEmotionSadness: UIImage = UIImage(named: "bg_sadness")!
+    public static let bgEmotionJoy: UIImage = UIImage(named: "bg_joy")!
+    public static let bgEmotionSoso: UIImage = UIImage(named: "bg_soso")!
+    public static let bgEmotionSurprise: UIImage = UIImage(named: "bg_surprise")!
+    
+    public static let imgEmotionAngry: UIImage = UIImage(named: "img_angry")!
+    public static let imgEmotionFear: UIImage = UIImage(named: "img_fear")!
+    public static let imgEmotionHate: UIImage = UIImage(named: "img_hate")!
+    public static let imgEmotionSadness: UIImage = UIImage(named: "img_sadness")!
+    public static let imgEmotionJoy: UIImage = UIImage(named: "img_joy")!
+    public static let imgEmotionSoso: UIImage = UIImage(named: "img_soso")!
+    public static let imgEmotionSurprise: UIImage = UIImage(named: "img_surprise")!
+    
+    public static let icPlay: UIImage = UIImage(named: "ic_play")!
+    public static let icPlus: UIImage = UIImage(named: "ic_plus")!
+}
