780247fb3473a56bf4b14193bf7e9f0e3290ee6a
bsorinnn <bxxsxrxx@gmail.com>
2022-04-23 23:58:51 +0900

    [FIX] 피드백 뷰 오류 고침(#10)

--- HaluEumpyo/HaluEumpyo/Presentation/Home/FeedBackPopUpViewController.swift
+++ HaluEumpyo/HaluEumpyo/Presentation/Home/FeedBackPopUpViewController.swift
@@ -29,11 +29,95 @@ final class FeedBackPopUpViewController: BaseViewController {
         $0.spacing = 20
     }
     
+    private let cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.titleLabel?.font = .pretendard(size: 16)
+        $0.setTitleColor(UIColor.lightGray, for: .normal)
+        $0.addTarget(self, action: #selector(cancelButtonDidTap), for: .touchUpInside)
+    }
+    
+    let sendButton = UIButton().then {
+        $0.isEnabled = false
+        $0.setTitle("피드백 보내기", for: .normal)
+        $0.titleLabel?.font = .pretendard(size: 16)
+        $0.setTitleColor(UIColor.haluEumpyo_purple(), for: .normal)
+        $0.setTitleColor(UIColor.gray003(), for: .disabled)
+        $0.addTarget(self, action: #selector(confirmButtonDidTap), for: .touchUpInside)
+    }
+    
+    weak var delegate: PopUpActionProtocol?
+    var rateButtonList: [UIButton] = []
+    var starRate = BehaviorSubject<Int>(value: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func configUI() {
         super.configUI()
+        view.backgroundColor = UIColor.haluEumpyo_background()
+    }
+    
+    override func setUpLayoutConstraint() {
+        view.addSubview(containerView)
+        containerView.addSubviews([titleLabel, rateStackView, cancelButton, sendButton])
+        
+        for index in 1...5 {
+            let button = HERateButton(type: .rate)
+            button.setTitle("\(index)", for: .normal)
+            button.tag = index
+            button.addTarget(self, action: #selector(self.setAction(sender:)), for: .touchUpInside)
+            rateButtonList.append((button))
+            self.rateStackView.addArrangedSubview(button)
+        }
+        
+        containerView.snp.makeConstraints {
+            $0.width.equalTo(320)
+            $0.height.equalTo(428)
+            $0.center.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        rateStackView.snp.makeConstraints {
+            $0.height.equalTo(40)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.width.equalTo(160)
+            $0.leading.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(28)
+        }
+        
+        sendButton.snp.makeConstraints {
+            $0.width.equalTo(160)
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(28)
+        }
+    }
+    
+    @objc
+    private func setAction(sender: UIButton) {
+        let ratingScore = sender.tag
+        print(ratingScore)
+        rateButtonList.enumerated().forEach {
+            $1.isSelected = ratingScore == $0 + 1 ? true : false
+        }
+        starRate.onNext(ratingScore)
+    }
+    
+    @objc
+    func cancelButtonDidTap() {
+        delegate?.cancelButtonDidTap(cancelButton)
+    }
+    
+    @objc
+    func confirmButtonDidTap() {
+        delegate?.confirmButtonDidTap(sendButton)
     }
 }
