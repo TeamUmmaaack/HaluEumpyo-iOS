ee3a700c8a3a7c60b73f3f1ad942a9ceca27a10c
bsorinnn <bxxsxrxx@gmail.com>
2022-04-25 00:00:05 +0900

    [CHORE] Extensions 수정 (#10)

--- HaluEumpyo/HaluEumpyo/Extensions/Date+Extension.swift
+++ HaluEumpyo/HaluEumpyo/Extensions/Date+Extension.swift
@@ -10,15 +10,54 @@ import Foundation
 import Then
 
 enum DateFormat: String {
-    case format1 = "yyyy.MM.dd(EE)"
+    case format1 = "yyyy.MM.dd"
     case format2 = "dd(EE)"
 }
 
 extension Date {
+    enum FormatType {
+        case full
+        case year
+        case day
+        case month
+        case second
+        case time
+        case calendar
+        case calendarTime
+        
+        var description: String {
+            switch self {
+            case .full:
+                return "yyyy-MM-dd'T'HH:mm:ss.SSS'Z"
+            case .year:
+                return "yyyy-MM-dd"
+            case .day:
+                return "M월 d일 EEEE"
+            case .month:
+                return "M월"
+            case .second:
+                return "HH:mm:ss"
+            case .time:
+                return "a h:mm"
+            case .calendar:
+                return "yyyy년 MM월 dd일"
+            case .calendarTime:
+                return "a HH:mm"
+            }
+        }
+    }
+    
+    func toString(of type: FormatType) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = type.description
+        return dateFormatter.string(from: self)
+    }
+
     func formatter(_ format: DateFormat) -> DateFormatter {
         return DateFormatter().then {
             $0.dateFormat = format.rawValue
-            $0.locale = Locale(identifier: "eng")
+            $0.locale = Locale(identifier: "ko_KR")
         }
     }
     
--- HaluEumpyo/HaluEumpyo/Extensions/UILabel+Extension.swift
+++ HaluEumpyo/HaluEumpyo/Extensions/UILabel+Extension.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 extension UILabel {
+    func setLineHeight(_ lineHeight: CGFloat) {
+        if let text = self.text {
+            let style = NSMutableParagraphStyle()
+            style.maximumLineHeight = lineHeight
+            style.minimumLineHeight = lineHeight
+            style.alignment = self.textAlignment
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .paragraphStyle: style,
+                .baselineOffset: (lineHeight - font.lineHeight) / 4
+            ]
+                
+            let attrString = NSAttributedString(string: text,
+                                                attributes: attributes)
+            self.attributedText = attrString
+       
+        }
+    }
+    
      func setTextWithLineHeight(text: String?, lineHeight: CGFloat){
         if let text = text {
             let style = NSMutableParagraphStyle()
