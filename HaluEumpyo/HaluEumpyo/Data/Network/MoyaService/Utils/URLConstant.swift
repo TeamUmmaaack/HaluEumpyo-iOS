0636df0e5cd682e09bdf71419ca007e8104665ce
bsorinnn <bxxsxrxx@gmail.com>
2022-06-07 00:18:08 +0900

    [FIX] url 주소 수정 (#16)

--- HaluEumpyo/HaluEumpyo/Data/Network/MoyaService/Utils/URLConstant.swift
+++ HaluEumpyo/HaluEumpyo/Data/Network/MoyaService/Utils/URLConstant.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum URLConstant {
-    static let baseURL = "https://asia-northeast3-haluempyo.cloudfunctions.net/api/"
+    static let baseURL = "https://asia-northeast3-haluempyo.cloudfunctions.net/api"
     
     static let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjEsImVtYWlsIjoidGVzdGVtYWlsMUBnbWFpbC5jb20iLCJpZEZpcmViYXNlIjoiYTM0SEduSXRQbmFCUllxOUkyNFhZTGZmWnhFMiIsImlhdCI6MTY1NDAyNjM1MCwiZXhwIjoxNjU1MjM1OTUwLCJpc3MiOiJoYWx1ZXVtcHlvIn0._7FH9kiGcfsyuvdAmHfbs29vm3SVOZTjvtkIYVRKuWU"
     
