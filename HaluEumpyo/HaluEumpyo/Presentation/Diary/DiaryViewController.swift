fb0b5b8121f3b53c263e4923ec0cd385ba81880f
bsorinnn <bxxsxrxx@gmail.com>
2022-07-08 22:03:15 +0900

    fix:  textView scrollable하게 수정(#31)
    - 키보드 높이에 따라 textView scrollable하게 수정

--- HaluEumpyo/HaluEumpyo/Presentation/Diary/DiaryViewController.swift
+++ HaluEumpyo/HaluEumpyo/Presentation/Diary/DiaryViewController.swift
@@ -31,6 +31,7 @@ class DiaryViewController: BaseViewController {
     
     private let contentTextView = UITextView().then {
         $0.font = .pretendard(.regular, size: 15)
+        $0.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         $0.textColor = UIColor.haluEmpyo_black()
         $0.isEditable = false
     }
